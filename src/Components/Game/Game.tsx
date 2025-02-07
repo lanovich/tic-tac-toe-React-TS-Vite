8fb3d2c510656dbb24148b943990a37e7bf7352e
lanovich <kolaemelanov60@gmail.com>
2025-02-07 13:17:00 +0300

    added tailwind and class components

--- src/Components/Game/Game.tsx
+++ src/Components/Game/Game.tsx
@@ -1,22 +1,34 @@
-import styles from "./Game.module.css";
-import { Field, Information } from "./";
-import { useDispatch } from "react-redux";
+import { Component } from "react";
 import { restartGame } from "../../actions";
+import { connect } from "react-redux";
+import { ConnectedField } from "../Field/Field";
+import { ConnectedInformation } from "../Information/Information";
 
-export const Game = () => {
-  const dispatch = useDispatch();
+interface GameProps {
+  restartGame: () => void;
+}
 
-  const handleRestartGame = () => {
-    dispatch(restartGame());
+class Game extends Component<GameProps> {
+  handleRestartGame = () => {
+    this.props.restartGame();
   };
 
-  return (
-    <div className={styles.mainContainer}>
-      <Information />
-      <Field />
-      <button onClick={handleRestartGame}>Restart</button>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="min-h-[90%] min-w-[90%] w-[50vh] h-[60vh] bg-[#c56caf] shadow-[8px_8px_10px_5px_rgba(0,0,0,0.1)] text-center p-[15px] rounded-[20px]">
+        <ConnectedInformation />
+        <ConnectedField />
+        <button
+          className="cursor-pointer bg-[rgb(168,110,185)] w-[100px] shadow-[1px_1px_5px_0px] mt-[10vh] p-[5px] rounded-[5px] border-[none] hover:transition-[0.5s] hover:bg-[rgb(161,32,201)] active:shadow-[inset_0_0_5px_rgba(0,0,0,0.5)]"
+          onClick={this.handleRestartGame}
+        >
+          Restart
+        </button>
+      </div>
+    );
+  }
+}
 
-export default Game;
+const mapDispatchToPropsGame = { restartGame };
+
+export default connect(null, mapDispatchToPropsGame)(Game);
